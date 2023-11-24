93e517ba8937d057dd080b49f33291065fc4e3ee
FelipeZambelloSv <rm99843@gmail.com.br>
2023-11-24 13:22:02 -0300

    finalizando return  header com os devidos links

--- src/components/header/page.jsx
+++ src/components/header/page.jsx
@@ -15,4 +15,53 @@ export default function Header() {
   
     const isAuthenticated = sessionStorage.getItem('isAuthenticated') === 'true';
   
+    return (
+        <div className={styles.header}>
+          <Link href="/">
+            <Image src="/img/watch.svg" width={50} height={50} alt="Logo" />
+          </Link>
+          <ul>
+            {isAuthenticated ? (
+              <>
+                <li>
+                  <Link href="/routes/curiosidade">Curiosidades</Link>
+                </li>
+                <li>
+                  <Link href="/routes/sobre">Sobre</Link>
+                </li>
+                <li>
+                  <Link href="/routes/objetivos">Meu Processo</Link>
+                </li>
+                <li>
+                  <Link href="/routes/feedback">Feedback</Link>
+                </li>
+                <li>
+                  <button className={styles.logout} onClick={handleLogout}>
+                    Logout
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link href="/routes/curiosidade">Curiosidades</Link>
+                </li>
+                <li>
+                  <Link href="/routes/sobre">Sobre</Link>
+                </li>
+                <li>
+                  <Link href="/routes/feedback">Feedback</Link>
+                </li>
+                <li>
+                  <Link href="/routes/login">Login</Link>
+                </li>
+                <li className={styles.cadastrar}>
+                  <Link href="/routes/cadastro">Cadastre-se</Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      );
+    }
     
