fc53a7f133d63fde0181444daee514cb08b24dc6
FelipeZambelloSv <rm99843@gmail.com.br>
2023-11-24 13:20:50 -0300

    Criandoo autentificação header

--- src/components/header/page.jsx
+++ src/components/header/page.jsx
@@ -4,3 +4,15 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import styles from './header.module.css';
 
+export default function Header() {
+    const router = useRouter();
+  
+    const handleLogout = () => {
+      sessionStorage.removeItem('isAuthenticated');
+      router.push('/');
+      router.refresh();
+    };
+  
+    const isAuthenticated = sessionStorage.getItem('isAuthenticated') === 'true';
+  
+    
