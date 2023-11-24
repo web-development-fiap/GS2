4a3e9bc91d79e77401910500df17f90847b529be
FelipeZambelloSv <rm99843@gmail.com.br>
2023-11-24 13:09:52 -0300

    adicionando nome dos desenvolvedores no footer

--- src/app/footer/page.jsx
+++ src/app/footer/page.jsx
@@ -2,4 +2,17 @@
 import React from 'react';
 import styles from './footer.module.css';
 
-
+export default function Footer() {
+    const students = [
+      { name: 'Luana Duque', rm: 'RM550813', role: 'Back-end' },
+      {
+        name: 'Mary Araújo Moreira Speranzini',
+        rm: 'RM550242',
+        role: 'Desenvolvedor de Software de IA',
+      },
+      { name: 'Elen Cabral', rm: 'RM98790', role: 'Back-end' },
+      { name: 'Felipe Zambello Silva', rm: 'RM99843', role: 'Banco de Dados' },
+      { name: 'Luigi Exposito Uchiyama', rm: 'RM99520', role: 'Front-end' },
+    ];
+  
+   
