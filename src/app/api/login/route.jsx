cd9d9425e5ad28e8a587925776fdeba18c54bc34
luanaduque <luanaduque273@gmail.com>
2023-11-24 18:55:41 -0300

    fazendo route.jsx login

--- src/app/api/login/route.jsx
+++ src/app/api/login/route.jsx
@@ -9,3 +9,26 @@ export async function GET(request, { params }) {
   const lista = await JSON.parse(file);
   return NextResponse.json(lista);
 }
+
+const handleLogin = async (email, senha) => {
+    const file = await fs.readFile(
+      process.cwd() + '/src/app/api/usuarios/db.json',
+      'utf8',
+    );
+    const dados = await JSON.parse(file);
+  
+    try {
+      const user = dados.usuarios.find(
+        (user) => user.email === email && user.senha === senha,
+      );
+  
+      if (user) {
+        return NextResponse.json({ status: true, user: user });
+      } else {
+        return NextResponse.json({ status: false, message: 'Login inválido' });
+      }
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json({ status: false, message: 'Erro no servidor' });
+    }
+  };
