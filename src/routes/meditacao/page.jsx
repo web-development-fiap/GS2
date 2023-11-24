8c73963c11d9355c85d54cbc129f0fbafe113091
crushedbyamallet <RM550242@fiap.com.br>
2023-11-24 20:11:23 -0300

    meditation page2

--- src/routes/meditacao/page.jsx
+++ src/routes/meditacao/page.jsx
@@ -20,3 +20,56 @@ const Meditacao = () => {
   const handleBackClick = () => {
     window.history.back();
   };
+
+  return (
+    <div className={styles.background}>
+      <div className={styles.container}>
+        <h2>Você já experimentou meditar?</h2>
+
+        <p
+          className={`${styles.labelButton} ${
+            selectedOption === 'jaExperimentei' && styles.selected
+          }`}
+          onClick={() => handleCheckboxChange('jaExperimentei')}
+        >
+          Sim, já experimentei
+        </p>
+        <p
+          className={`${styles.labelButton} ${
+            selectedOption === 'queroSaberMais' && styles.selected
+          }`}
+          onClick={() => handleCheckboxChange('queroSaberMais')}
+        >
+          Sim, mas gostaria de saber mais
+        </p>
+        <p
+          className={`${styles.labelButton} ${
+            selectedOption === 'muitoTempoAtras' && styles.selected
+          }`}
+          onClick={() => handleCheckboxChange('muitoTempoAtras')}
+        >
+          Sim, há muito tempo atrás
+        </p>
+        <p
+          className={`${styles.labelButton} ${
+            selectedOption === 'nuncaExperimentei' && styles.selected
+          }`}
+          onClick={() => handleCheckboxChange('nuncaExperimentei')}
+        >
+          Não, nunca experimentei meditar
+        </p>
+
+        <div className={styles.buttonContainer}>
+          <button className={styles.backButton} onClick={handleBackClick}>
+            Voltar
+          </button>
+          <button className={styles.nextButton} onClick={handleNextClick}>
+            Próximo
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Meditacao;
